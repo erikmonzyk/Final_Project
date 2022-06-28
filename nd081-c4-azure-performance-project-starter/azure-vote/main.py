c09afe5ccd66af25affea39882663e41c86b5fec
Monzyk, Erik (em7272) <erikmonzyk@att.com>
2022-06-28 16:29:00 -0500

    edited line 131 of main.py

--- nd081-c4-azure-performance-project-starter/azure-vote/main.py
+++ nd081-c4-azure-performance-project-starter/azure-vote/main.py
@@ -128,4 +128,4 @@ if __name__ == "__main__":
     # TODO: Use the statement below when running locally
     # app.run() 
     # TODO: Use the statement below before deployment to VMSS
-    (host='0.0.0.0', threaded=True, debug=True) # remote
+    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
